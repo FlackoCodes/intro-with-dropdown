baba16fd3297d5a297c18252cd86953ff647fb41
Flacko <lesliejoe2k2@gmail.com>
2024-05-16 08:08:46 +0000

    mobile version

---
+++ script.js
@@ -0,0 +1,42 @@
+
+const mobileBtn = document.querySelector(".mobile-btn");
+
+const closeBtn = document.querySelector(".close-btn");
+
+const navItems = document.querySelector(".nav-list-items-mobile");
+
+// const subFeatBtn = document.querySelector('.feat');
+// const subCompBtn = document.querySelector('.comp');
+
+// const hiddenFeat = document.querySelector('.sub-feat');
+// const hiddenComp = document.querySelector('.sub-comp');
+
+
+// subFeatBtn.addEventListener('click', ()=>{
+//     hiddenFeat.classList.toggle('active-nav-menu')
+// })
+// subCompBtn.addEventListener('click', ()=>{
+//     hiddenComp.classList.toggle('active-nav-menu')
+// })
+
+function toggleMenu(buttonSelector, menuSelector) {
+    const button = document.querySelector(buttonSelector);
+    const menu = document.querySelector(menuSelector);
+
+    button.addEventListener('click', (event) => {
+        event.preventDefault();
+        menu.classList.toggle('active-nav-menu');
+    });
+}
+
+toggleMenu('.feat', '.sub-feat');
+toggleMenu('.comp', '.sub-comp');
+
+
+const mobileMenuOpen = () =>{
+    navItems.classList.toggle("active-nav-menu")
+}
+
+
+mobileBtn.addEventListener('click', mobileMenuOpen);
+closeBtn.addEventListener("click", mobileMenuOpen);
