fbc3ba76599a8058b2782daf008d285dbece611c
Flacko <lesliejoe2k2@gmail.com>
2024-06-07 15:58:09 +0000

    rebuilt mobile version

--- script.js
+++ script.js
@@ -1,28 +1,13 @@
-const mobileBtn = document.querySelector(".mobile-btn");
-
+const mobileBtn = document.querySelector(".mobile-open");
 const closeBtn = document.querySelector(".close-btn");
+const navItems = document.querySelector(".nav-list-mobile");
 
-const navItems = document.querySelector(".nav-list-items-mobile");
+const toggleMobileMenu = () =>{
+    navItems.classList.toggle("active-nav-menu")
+}
 
-
-const arrowUp = document.querySelector('.arrow-up')
-const arrowDown = document.querySelector('.arrow-down')
-
-
-
-// const subFeatBtn = document.querySelector('.feat');
-// const subCompBtn = document.querySelector('.comp');
-
-// const hiddenFeat = document.querySelector('.sub-feat');
-// const hiddenComp = document.querySelector('.sub-comp');
-
-
-// subFeatBtn.addEventListener('click', ()=>{
-//     hiddenFeat.classList.toggle('active-nav-menu')
-// })
-// subCompBtn.addEventListener('click', ()=>{
-//     hiddenComp.classList.toggle('active-nav-menu')
-// })
+mobileBtn.addEventListener('click', toggleMobileMenu);
+closeBtn.addEventListener("click", toggleMobileMenu);
 
 function toggleMenu(buttonSelector, menuSelector) {
     const button = document.querySelector(buttonSelector);
@@ -36,12 +21,6 @@ function toggleMenu(buttonSelector, menuSelector) {
 toggleMenu('.feat', '.sub-feat');
 toggleMenu('.comp', '.sub-comp');
 
-
-
-const mobileMenuOpen = () =>{
-    navItems.classList.toggle("active-nav-menu")
-}
-
-
-mobileBtn.addEventListener('click', mobileMenuOpen);
-closeBtn.addEventListener("click", mobileMenuOpen);
+document.querySelector('.feat').addEventListener('click', ()=>{
+    console.log('feat clicked');
+})
